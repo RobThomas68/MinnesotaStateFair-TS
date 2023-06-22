ce1610f694e8ce4233d9c195107b9ccb53343872
Rob Thomas <robthomas68@gmail.com>
2023-06-22 00:26:21 -0400

    added firestore and basic save/load of favorites

--- src/components/Favorites.tsx
+++ src/components/Favorites.tsx
@@ -1,13 +1,47 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import {
+  FavoriteArrayType,
   FavoriteContext,
   FavoriteContextType,
 } from "../context/FavoriteProvider";
 import FavoriteFeed from "./FavoriteFeed";
 
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { db } from "../config/Firestore";
+
 const Favorites = () => {
-  const { favoriteSearchResults, favoriteSearch, setFavoriteSearch } =
-    useContext(FavoriteContext) as FavoriteContextType;
+  const {
+    favorites,
+    setFavorites,
+    favoriteSearchResults,
+    favoriteSearch,
+    setFavoriteSearch,
+  } = useContext(FavoriteContext) as FavoriteContextType;
+
+  const [user, setUser] = useState("");
+
+  const handleSaveClick = async () => {
+    if (user) {
+      console.log(user, favorites);
+      await setDoc(doc(db, "users", user), { favorites });
+    }
+  };
+
+  const handleLoadClick = async () => {
+    if (user) {
+      const docRef = doc(db, "users", user);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+        const favs = docSnap.data().favorites as FavoriteArrayType;
+        console.log("favs:", favs);
+        setFavorites(favs);
+      } else {
+        console.log("No such document!");
+      }
+    }
+  };
 
   return (
     <>
@@ -23,6 +57,25 @@ const Favorites = () => {
           />
         </form>
       </div>
+
+      <div>
+        <input
+          type="text"
+          id="user"
+          name="user"
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+        />
+
+        <button type="button" onClick={handleSaveClick}>
+          Save
+        </button>
+
+        <button type="button" onClick={handleLoadClick}>
+          Load
+        </button>
+      </div>
+
       <main className="Favorites">
         {favoriteSearchResults.length ? (
           <FavoriteFeed feedItems={favoriteSearchResults} />
